29eae2db65406e877627ec48a80bb18c5124e371
kkmeesho <100832169+kkmeesho@users.noreply.github.com>
2022-10-04 13:25:55 +0530

    updated new list

--- src/constants/thinkify.constants.js
+++ src/constants/thinkify.constants.js
@@ -21,6 +21,17 @@ const THINKIFY_LIST = [
         availability: "Immediate",
         gitHub: 'https://github.com/thinkify',
     },
+    {
+        id:"3",
+        name: "modi",
+        role: "Recruiter",
+        skills: "Talent acquisition",
+        experience: "4 years",
+        rate : "$8/hr",
+        location: "Bangalore",
+        availability: "Immediate",
+        gitHub: 'https://github.com/thinkify',
+    },
 
 ]
 export default THINKIFY_LIST;
